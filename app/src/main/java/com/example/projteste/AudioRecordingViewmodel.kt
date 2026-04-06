0ceff08f5e76385e504cf724f4cefbac080493fd
felipelucio <felipelucio2009@gmail.com>
2026-04-06 20:14:24 -0300

    feat: implementa classe de viewmodel

--- app/src/main/java/com/example/projteste/AudioRecordingViewmodel.kt
+++ app/src/main/java/com/example/projteste/AudioRecordingViewmodel.kt
@@ -1,6 +1,42 @@
 package com.example.projteste
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class AudioRecordingViewmodel : ViewModel() {
+class AudioRecordingViewModel(
+    private val repository: AudioRecordingRepository
+) : ViewModel() {
+
+    private val _isRecording = MutableStateFlow(false)
+    val isRecording: StateFlow<Boolean> = _isRecording
+
+    private val _recordingSuccess = MutableStateFlow<Result<String>?>(null)
+    val recordingSuccess: StateFlow<Result<String>?> = _recordingSuccess
+
+    fun startRecording() {
+        viewModelScope.launch {
+            try {
+                repository.startRecording()
+                _isRecording.value = true
+            } catch (e: Exception) {
+                _recordingSuccess.value = Result.failure(e)
+            }
+        }
+    }
+
+    fun finalizeRecording() {
+        viewModelScope.launch {
+            try {
+                val path = repository.stopAndSave()
+                _isRecording.value = false
+                _recordingSuccess.value = Result.success(path)
+            } catch (e: Exception) {
+                _isRecording.value = false
+                _recordingSuccess.value = Result.failure(e)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/projteste/di/AppModule.kt
+++ app/src/main/java/com/example/projteste/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.example.projteste.di
 
 import com.example.projteste.AudioRecordingRepository
+import com.example.projteste.AudioRecordingViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { AudioRecordingRepository(androidContext()) }
+    viewModel { AudioRecordingViewModel(get()) }
 }
